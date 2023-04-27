5496e1a6676cbe3602d60f249a633f79e6803311
Won Yang <yangwon700@gmail.com>
2023-04-27 18:07:18 -0400

    change config to environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 // Pages
 import LoginPage from "./pages/LoginPage";
-
-// Import the functions you need from the SDKs you need
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  connectAuthEmulator,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCWz5mwcTQ1AJj9Mbim5QT2pPdsAAF9_XE",
-  authDomain: "the-swear-jar-1.firebaseapp.com",
-  projectId: "the-swear-jar-1",
-  storageBucket: "the-swear-jar-1.appspot.com",
-  messagingSenderId: "297263056849",
-  appId: "1:297263056849:web:edb1b5d3b9a785739642f0",
-  measurementId: "G-GBYSM2B8C2",
-};
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Authentication
-const auth = getAuth(firebaseApp);
-connectAuthEmulator(auth, "http://localhost:9099");
-
-// React Router
 
 function App() {
   return (
---
+++ src/config/firebase.ts
@@ -0,0 +1,34 @@
+// Import the functions you need from the SDKs you need
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  connectAuthEmulator,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = process.env;
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+// Authentication
+const auth = getAuth(firebaseApp);
+connectAuthEmulator(auth, "http://localhost:9099");
